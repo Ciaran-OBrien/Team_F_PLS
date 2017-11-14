a300ecc27d98cae9704b0a8a61dcdd138431d852
Ciaran-OBrien <c15765215@mydit.ie>
2017-11-14 22:05:03 +0100

    LYNN. Adding the courier class

---
+++ src/main/java/Person/Courier.java
@@ -0,0 +1,33 @@
+/*
+ * Author: Lynn
+ * Lecture: Bennard Humm
+ */
+package Person;
+
+/**
+ *
+ * @author Lynn_Ngx
+ */
+public class Courier implements IPerson {
+    
+    public String Name;
+    public String Address;
+
+   @Override
+    public void setName() {}
+
+    @Override
+    public String getName() {
+        return null;
+    }
+
+    @Override
+    public void setAddres() {}
+
+    @Override
+    public String getAddress() {
+        return null;
+    }
+    
+    
+}
