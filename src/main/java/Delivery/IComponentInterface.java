ff77f2be68b4ce3107975e0d3824b3cc150a6eda
Ciaran-OBrien <c15765215@mydit.ie>
2017-11-14 21:59:35 +0100

    LUKE&TOM. Adding the Delivery interface.

---
+++ src/main/java/Delivery/IComponentInterface.java
@@ -0,0 +1,17 @@
+/*
+ * Authors: Luke & Tom
+ * Lecture: Bennard Humm
+ */
+package Delivery;
+
+/**
+ *
+ * @authors Luke & Tom
+ */
+public interface IComponentInterface {
+    public int getID();
+    public int getParcelID();
+    public int getCourierID();
+    public String toString();
+    
+}
