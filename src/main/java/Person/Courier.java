6cdcacd27f8fa70d5397408e35e4307b4eb244ab
Shonamd <shonamdoran@gmail.com>
2017-11-21 21:35:26 +0100

    Update Courier.java

--- src/main/java/Person/Courier.java
+++ src/main/java/Person/Courier.java
@@ -10,39 +10,11 @@ package Person;
  */
 public abstract class Courier implements IPerson {
     
-    public String Name;
-    public String Address;
-
-    /**
-     * This method will set the name of the courier
-     * @param name
-     */
-   @Override
-    public void setName(String name) {}
-
-    /**
-     * This method will retrieve the name of the courier
-     * @return string Courier name
-     */
-    @Override
-    public String getName() {
-        return null;
-    }
-
-    /**
-     * This method will set the address of the courier
-     * @param address
-     */
-    @Override
-    public void setAddres(String address) {}
-
-    /**
-     * This method will return the address of the courier
-     * @return string Courier address
-     */
-    @Override
-    public String getAddress() {
-        return null;
+    public int CourierId = 0 ;
+    
+    public int setCourierId(int CourierId){
+        this.CourierId = CourierId ++ ;
+        return CourierId ;
     }
     
     
