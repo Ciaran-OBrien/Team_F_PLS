50fdcd4fede98931730109d630a0deded0bef4be
Ciaran-OBrien <c15765215@mydit.ie>
2017-11-26 19:41:36 +0100

    Added CustomerStatus

--- src/main/java/Person/Customer.java
+++ src/main/java/Person/Customer.java
@@ -3,7 +3,7 @@
  * Lecture: Bennard Humm
  */
 package Person;
-
+import Status.OrderStatus;
 /**
  *
  * @authors 
@@ -14,9 +14,21 @@ public class Customer extends AbstractPerson{
     public Customer(long ID, String Name, String Address) {
         super(ID, Name, Address);
     }
+    
+    private OrderStatus customerStatus;
+    
+    
 
     @Override
     public String toString() {
         return "";
+    }
+
+    public OrderStatus getCustomerStatus() {
+        return customerStatus;
+    }
+
+    public void setCustomerStatus(OrderStatus customerStatus) {
+        this.customerStatus = customerStatus;
     }
 }
